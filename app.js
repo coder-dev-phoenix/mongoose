87fe0400497d8bb295cc28724221f4ebe67f79d6
abbassiddiqui99 <abbassiddiqui99@gmail.com>
2020-02-25 17:20:16 +0500

    mongoose add,validate and find method

--- app.js
+++ app.js
@@ -6,55 +6,74 @@ mongoose.connect("mongodb://localhost/testDB", {
 });
 
 const testSchema = new mongoose.Schema({
-	name: String,
-	age: Number,
+	name: {
+		type: String,
+		required: [true, "What is your name?"]
+	},
+	age: {
+		type: Number,
+		min: 1,
+		max: 100
+	},
 	education: String
 });
 
 const testModel = mongoose.model("testStore", testSchema);
 
 const testValue = new testModel({
-	name: "abbas",
-	age: 21,
+	name: "hassan",
+	age: 121,
 	education: "BSCS"
 });
 
-// testValue.save();
+testValue.save();
 
-const personSchema = new mongoose.Schema({
-	name: String,
-	age: Number
-});
+// const personSchema = new mongoose.Schema({
+// 	name: String,
+// 	age: Number
+// });
 
-const personModel = mongoose.model("personstore", personSchema);
+// const personModel = mongoose.model("personstore", personSchema);
 
-const personValue = new personModel({
-	name: "John",
-	age: 37
-});
+// const personValue = new personModel({
+// 	name: "John",
+// 	age: 37
+// });
 
 // personValue.save();
 
-const testValue01 = new testModel({
-	name: "ammad",
-	age: 24,
-	education: "ACCA"
-});
-const testValue02 = new testModel({
-	name: "bilal",
-	age: 27,
-	education: "DAE"
-});
-const testValue03 = new testModel({
-	name: "maaz",
-	age: 17,
-	education: "INT"
-});
+// const testValue01 = new testModel({
+// 	name: "ammad",
+// 	age: 24,
+// 	education: "ACCA"
+// });
+// const testValue02 = new testModel({
+// 	name: "bilal",
+// 	age: 27,
+// 	education: "DAE"
+// });
+// const testValue03 = new testModel({
+// 	name: "maaz",
+// 	age: 17,
+// 	education: "INT"
+// });
 
-testModel.insertMany([testValue01, testValue02, testValue03], function(err) {
-	if (err) {
-		console.log(err);
-	} else {
-		console.log("Succesfully save your data");
-	}
-});
+// testModel.insertMany([testValue01, testValue02, testValue03], function(err) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+// 		console.log("Succesfully save your data");
+// 	}
+// });
+
+// testModel.find(function(err, personstores) {
+// 	if (err) {
+// 		console.log(err);
+// 	} else {
+//         // console.log(personstores);
+// 		personstores.forEach(function(value) {
+// 			console.log(value.name);
+//         });
+//         mongoose.connection.close()
+//     }
+// });
